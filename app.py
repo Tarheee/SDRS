b017230c1499e14da95113deb6ada1e24b064735
Tarhe <2832190426@qq.com>
2025-04-11 13:53:27 +0800

    add the app.py and /templates/index.html

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for
+from PIL import Image
+import numpy as np
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ =='__main__':
+    app.run(port=8000,debug=True)
